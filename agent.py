fb9ccf306b8457a69430258c1a1b44e0e4455d2a
Bjørn Kåre Sæbø <bjornksa@stud.ntnu.no>
2020-11-16 14:33:45 +0100

    Add Keras stuff to the thing

--- agent.py
+++ agent.py
@@ -1,15 +1,18 @@
 import random
 import numpy as np
-import keras
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Convolution2D, Dropout, Activation, Flatten
 
 class Agent():
 
     def __init__(
                 self,
                 gamma=0.9,
-                epsilon=0.9,
+                epsilon=1,
                 num_training_games=10000,
-                memory_size=5000
+                memory_size=5000,
+                board_size=(10, 20)
                 ):
 
         """
@@ -23,17 +26,23 @@ class Agent():
 
         num_traing_games: How many games should be spent traing
         before utilizing optimal learned strategy
+
+        memory_size: Amount of training episodes that should be stored in memory
+
+        board_size [tuple(x, y)]: dimension of tetris board matrix
+
         """
         self.gamma = gamma
         self.epsilon = epsilon
-        self._memory = np.array([])
-        self._model = self._build_model()
+        self._memory = []
         self._num_training_games = num_training_games
         self._max_memory_size = memory_size
         self._mem_index = 0
+        self._state_size = board_size
 
         self._epsilon_decrement = epsilon / num_training_games
 
+        self._model = self._build_model()
 
     def get_next_state(self, possible_next_states):
         if random.random() < self.epsilon:
@@ -43,7 +52,7 @@ class Agent():
             best_reward = 0
             best_action = 0
             for index, state in enumerate(possible_next_states):
-                predicted_reward = self._predict_reward(state)
+                predicted_reward = self._predict_reward(state[0])
                 if predicted_reward > best_reward:
                     best_reward = predicted_reward
                     best_action = index
@@ -51,7 +60,7 @@ class Agent():
         return best_action
 
     def add_to_memory(self, action, reward, is_game_over):
-        np.append(self._memory, (action, reward, is_game_over, self._mem_index))
+        self._memory.append([action, reward, is_game_over, self._mem_index])
         self._mem_index += 1
 
         if self._mem_index > self._max_memory_size:
@@ -61,7 +70,17 @@ class Agent():
     def _build_model(self):
         #TODO: Her initaliseres nettverket som skal vurdere hvor bra et flytt er
         # model = keras.Model(), model.Add()..., model.compile(), return model ish
-        return 0
+        # Første lag må ha
+        model = Sequential()
+
+        model.add(Convolution2D(4, 1, 1, input_shape=(1, *self._state_size)))
+        model.add(Flatten())
+        model.add(Dense(4, activation='relu'))
+        model.add(Dense(1, activation='linear'))
+
+        model.compile(optimizer="adam", loss='mse')
+
+        return model
 
 
     def train(self):
@@ -70,9 +89,9 @@ class Agent():
         self.epsilon -= self._epsilon_decrement
 
         sample_size = min(len(self._memory), 200) # Set sample_size to 200, or size of memory if len(memory) < 200
-        sample = np.random.choice(self._memory, sample_size) # Train on a random sample of the memory
+        sample = random.sample(self._memory, sample_size) # Train on a random sample of the memory
 
-        actions = np.array([episode[0] for episode in sample]) # X_values for network
+        actions = np.array( [np.array(episode[0]).reshape(-1, 1, 10, 20) for episode in sample] ) # X_values for network
 
         q_values = np.array([]) # Y_values for network
         for episode in sample:
@@ -81,14 +100,15 @@ class Agent():
             index = episode[3]
 
             if is_game_over:
-                np.append(q_values, reward)
+                q_values = np.append(q_values, reward)
             else:
                 next_episode = self._memory[index + 1]
                 next_q = self._predict_reward(next_episode[0])
                 q_value = reward + self.gamma * next_q
-                np.append(q_values, q_value)
+                q_values = np.append(q_values, q_value)
 
-        # self._model.fit(actions, q_values)
+        self._model.fit(actions, q_values,
+        batch_size=32, epochs=10, verbose=1)
 
 
     def _predict_reward(self, possible_next_state):
@@ -97,6 +117,6 @@ class Agent():
         output: Predicted Reward for the action
         """
         #TODO: Her skal nettverket brukes til å predikere hvor bra et enkelt flytt er
-        # return self.model.predict(possble_next_state)
-        return 1
+        possible_next_state = np.array(possible_next_state).reshape(-1, 1,10,20)
+        return self._model.predict(possible_next_state)
 
