a7a83ac349ed98636bf94aa8293399206c595d28
Erlend Blomseth <erlend@elendig.no>
2020-11-16 13:55:44 +0100

    Change state to board matrix with piece appended

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import environment
 import agent
+from tetris import Piece, get_board_with_piece_matrix
 
 env = environment.Environment(number_of_games = 10)
 agt = agent.Agent()
@@ -7,14 +8,20 @@ for i in range(env.number_of_games):
     #env.game.print() #debugger
     print(env.game_iterator) #debugger
     while not env.game.is_game_over():
-        board = env.game.get_board()
-        piece = env.game.get_piece()
+        board = env.game.board
+        piece_matrix = env.game.get_piece()
+        piece = Piece(piece_matrix)
+        
         possible_piece_placements = env.game.get_possible_piece_placements()
         #for p in possible_piece_placements: #debugger
         #    print(p) #debugger
         possible_next_states = []
         for piece_placement in possible_piece_placements:
-            possible_next_states.append((board, piece, piece_placement))
+            (x, y), rotation = piece_placement
+            piece.set_rotation(rotation)
+            piece.set_position(x, y)
+            state = get_board_with_piece_matrix(board, piece)
+            possible_next_states.append(state)
         next_state_id = agt.get_next_state(possible_next_states)
         env.place_piece(possible_next_states[next_state_id][2])
         env.tick()
