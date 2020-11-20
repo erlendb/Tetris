9ab4f7dcde3104089fc6086d2c5f80a5c2147eb6
Erlend Blomseth <erlend@elendig.no>
2020-11-21 00:04:08 +0100

    Add should_log_moves and should_train options in main + rename log_write() -> log_game()

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ log_extra_information = '' # Extra information to put in the log file
 load_model_name = '' # Name of the model you want to load. Leave blank if you want to build a new model.
 number_of_training_games = 2 # Games where the epsilon goes from 1 to 0.
 number_of_games_total = number_of_training_games + 2 # If this is higher than number_of_training_games, then the last games will be run with epsilon 0 (always use the moves with the best reward)
+should_log_moves = True # If True, all moves (piece placements) will be logged in a separate file.
+should_train = False
 ###
 
 agt = agent.Agent(num_training_games = number_of_training_games, saved_model_name = load_model_name)
@@ -17,6 +19,8 @@ env = environment.Environment(number_of_games = number_of_games_total)
 for i in range(env.number_of_games):
     #env.game.print() #debugger
     print(env.game_iterator) #debugger
+    if should_log_moves:
+        moves = []
     while not env.game.is_game_over():
         board = env.game.board
         piece_matrix = env.game.get_piece()
@@ -37,10 +41,15 @@ for i in range(env.number_of_games):
         env.tick()
         reward = env.get_reward()
         agt.add_to_memory(possible_next_states[next_state_id], reward)
-    env.log_write(model_name = model_name, extra_information = log_extra_information, epsilon = agt.epsilon)
+        if should_log_moves:
+            moves.append((piece.type, possible_piece_placements[next_state_id]))
+    env.log_game(model_name = model_name, extra_information = log_extra_information, epsilon = agt.epsilon)
+    if should_log_moves:
+        env.log_moves(model_name = model_name, moves=moves)
     env.reset_game()
 
-    agt.train()
+    if should_train:
+        agt.train()
     agt.clear_memory()
     
     if (i % 100) == 0:
